43b8661ef770d85d81584a3e7a9f0e154d2e437b
Suraj20 <bhosalesuraj2012@gmail.com>
2025-12-12 12:43:56 +0530

    commit changes

---
+++ server/middlewares/exception_handlers.py
@@ -0,0 +1,14 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from logger import logger
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        response = await call_next(request)
+        return response
+    except Exception as e:
+        logger.exception("Unhandled Exception")
+        return JSONResponse(
+            status_code=500,
+            content={"error": str(e)},
+        )
